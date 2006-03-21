328e3c5be636cfb3eacd2cc16d5d9d9ade9ad807
omschaub <omschaub@58cabe8c-710f-0410-8395-bf4839b10e17>
2006-03-21 02:07:56 +0000

    Fixing the imageRepository for the new name.. more to come.. but not before ANIME!  .. tomorrow
    git-svn-id: http://svn.code.sf.net/p/azsmrc/code/trunk@15 58cabe8c-710f-0410-8395-bf4839b10e17

--- azsmrc/lbms/azsmrc/remote/client/swtgui/ImageRepository.java
+++ azsmrc/lbms/azsmrc/remote/client/swtgui/ImageRepository.java
@@ -26,84 +26,84 @@ public class ImageRepository {
 
 	public static void loadImages(Display display) {
 
-		loadImage(display,"lbms/azmultiuser/remote/client/firefrog/resources/right_arrow.gif","right_arrow",255);
-		loadImage(display,"lbms/azmultiuser/remote/client/firefrog/resources/down_arrow.gif","down_arrow",255);
-		loadImage(display,"lbms/azmultiuser/remote/client/firefrog/resources/background.png","backgroundImage",255);
-		loadImage(display,"lbms/azmultiuser/remote/client/firefrog/resources/bar.png","barImage",255);
-		loadImage(display,"lbms/azmultiuser/remote/client/firefrog/resources/gg_connecting.gif","icon",255);
-		loadImage(display,"lbms/azmultiuser/remote/client/firefrog/resources/edit_add.png","plus",255);
+		loadImage(display,"lbms/azmultiuser/remote/client/swtgui/resources/right_arrow.gif","right_arrow",255);
+		loadImage(display,"lbms/azmultiuser/remote/client/swtgui/resources/down_arrow.gif","down_arrow",255);
+		loadImage(display,"lbms/azmultiuser/remote/client/swtgui/resources/background.png","backgroundImage",255);
+		loadImage(display,"lbms/azmultiuser/remote/client/swtgui/resources/bar.png","barImage",255);
+		loadImage(display,"lbms/azmultiuser/remote/client/swtgui/resources/gg_connecting.gif","icon",255);
+		loadImage(display,"lbms/azmultiuser/remote/client/swtgui/resources/edit_add.png","plus",255);
 
-		loadImage(display,"lbms/azmultiuser/remote/client/firefrog/resources/OpenFile.png","openfile",255);
+		loadImage(display,"lbms/azmultiuser/remote/client/swtgui/resources/OpenFile.png","openfile",255);
 
 		//Tray Icons
 		loadImage(display,"lbms/azmultiuser/remote/client/resources/TrayIcon_blue.png","TrayIcon_Blue",255);
 		loadImage(display,"lbms/azmultiuser/remote/client/resources/TrayIcon_red.png","TrayIcon_Red",255);
 		loadImage(display,"lbms/azmultiuser/remote/client/resources/TrayIcon_connecting.png","TrayIcon_Connecting",255);
 		//Torrent Move Icons
-		loadImage(display,"lbms/azmultiuser/remote/client/firefrog/resources/bottom.png","bottom",255);
-		loadImage(display,"lbms/azmultiuser/remote/client/firefrog/resources/down.png","down",255);
-		loadImage(display,"lbms/azmultiuser/remote/client/firefrog/resources/up.png","up",255);
-		loadImage(display,"lbms/azmultiuser/remote/client/firefrog/resources/top.png","top",255);
+		loadImage(display,"lbms/azmultiuser/remote/client/swtgui/resources/bottom.png","bottom",255);
+		loadImage(display,"lbms/azmultiuser/remote/client/swtgui/resources/down.png","down",255);
+		loadImage(display,"lbms/azmultiuser/remote/client/swtgui/resources/up.png","up",255);
+		loadImage(display,"lbms/azmultiuser/remote/client/swtgui/resources/top.png","top",255);
 
 		//Health Icons
-		//loadImage(display,"lbms/azmultiuser/remote/client/firefrog/resources/health/no_tracker.gif","health_blue",255);
-		//loadImage(display,"lbms/azmultiuser/remote/client/firefrog/resources/health/ok.gif","health_green",255);
-		//loadImage(display,"lbms/azmultiuser/remote/client/firefrog/resources/health/ko.gif","health_red",255);
-		//loadImage(display,"lbms/azmultiuser/remote/client/firefrog/resources/health/no_remote.gif","health_yellow",255);
-		//loadImage(display,"lbms/azmultiuser/remote/client/firefrog/resources/health/stopped.gif","health_gray",255);
-		loadImage(display,"lbms/azmultiuser/remote/client/firefrog/resources/Health_green.png","health_green",255);
-		loadImage(display,"lbms/azmultiuser/remote/client/firefrog/resources/Health_blue.png","health_blue",255);
-		loadImage(display,"lbms/azmultiuser/remote/client/firefrog/resources/Health_gray.png","health_gray",255);
-		loadImage(display,"lbms/azmultiuser/remote/client/firefrog/resources/Health_red.png","health_red",255);
-		loadImage(display,"lbms/azmultiuser/remote/client/firefrog/resources/Health_yellow.png","health_yellow",255);
+		//loadImage(display,"lbms/azmultiuser/remote/client/swtgui/resources/health/no_tracker.gif","health_blue",255);
+		//loadImage(display,"lbms/azmultiuser/remote/client/swtgui/resources/health/ok.gif","health_green",255);
+		//loadImage(display,"lbms/azmultiuser/remote/client/swtgui/resources/health/ko.gif","health_red",255);
+		//loadImage(display,"lbms/azmultiuser/remote/client/swtgui/resources/health/no_remote.gif","health_yellow",255);
+		//loadImage(display,"lbms/azmultiuser/remote/client/swtgui/resources/health/stopped.gif","health_gray",255);
+		loadImage(display,"lbms/azmultiuser/remote/client/swtgui/resources/Health_green.png","health_green",255);
+		loadImage(display,"lbms/azmultiuser/remote/client/swtgui/resources/Health_blue.png","health_blue",255);
+		loadImage(display,"lbms/azmultiuser/remote/client/swtgui/resources/Health_gray.png","health_gray",255);
+		loadImage(display,"lbms/azmultiuser/remote/client/swtgui/resources/Health_red.png","health_red",255);
+		loadImage(display,"lbms/azmultiuser/remote/client/swtgui/resources/Health_yellow.png","health_yellow",255);
 
 		//Statusbar icons
-		loadImage(display,"lbms/azmultiuser/remote/client/firefrog/resources/statusbar/connect_creating.png","connect_creating",255);
-		loadImage(display,"lbms/azmultiuser/remote/client/firefrog/resources/statusbar/connect_no.png","connect_no",255);
-		loadImage(display,"lbms/azmultiuser/remote/client/firefrog/resources/statusbar/connect_established.png","connect_established",255);
-		loadImage(display,"lbms/azmultiuser/remote/client/firefrog/resources/statusbar/decrypted_new.png","ssl_disabled",255);
-		loadImage(display,"lbms/azmultiuser/remote/client/firefrog/resources/statusbar/encrypted.png","ssl_enabled",255);
-        loadImage(display,"lbms/azmultiuser/remote/client/firefrog/resources/statusbar/statusbar_down.png","statusbar_down",255);
-        loadImage(display,"lbms/azmultiuser/remote/client/firefrog/resources/statusbar/statusbar_up.png","statusbar_up",255);
+		loadImage(display,"lbms/azmultiuser/remote/client/swtgui/resources/statusbar/connect_creating.png","connect_creating",255);
+		loadImage(display,"lbms/azmultiuser/remote/client/swtgui/resources/statusbar/connect_no.png","connect_no",255);
+		loadImage(display,"lbms/azmultiuser/remote/client/swtgui/resources/statusbar/connect_established.png","connect_established",255);
+		loadImage(display,"lbms/azmultiuser/remote/client/swtgui/resources/statusbar/decrypted_new.png","ssl_disabled",255);
+		loadImage(display,"lbms/azmultiuser/remote/client/swtgui/resources/statusbar/encrypted.png","ssl_enabled",255);
+        loadImage(display,"lbms/azmultiuser/remote/client/swtgui/resources/statusbar/statusbar_down.png","statusbar_down",255);
+        loadImage(display,"lbms/azmultiuser/remote/client/swtgui/resources/statusbar/statusbar_up.png","statusbar_up",255);
 
 		//Context menu icons
-		loadImage(display,"lbms/azmultiuser/remote/client/firefrog/resources/menu/stop.png","menu_stop",255);
-		loadImage(display,"lbms/azmultiuser/remote/client/firefrog/resources/menu/remove.png","menu_remove",255);
-		loadImage(display,"lbms/azmultiuser/remote/client/firefrog/resources/menu/recheck.png","menu_recheck",255);
-		loadImage(display,"lbms/azmultiuser/remote/client/firefrog/resources/menu/queue.png","menu_queue",255);
+		loadImage(display,"lbms/azmultiuser/remote/client/swtgui/resources/menu/stop.png","menu_stop",255);
+		loadImage(display,"lbms/azmultiuser/remote/client/swtgui/resources/menu/remove.png","menu_remove",255);
+		loadImage(display,"lbms/azmultiuser/remote/client/swtgui/resources/menu/recheck.png","menu_recheck",255);
+		loadImage(display,"lbms/azmultiuser/remote/client/swtgui/resources/menu/queue.png","menu_queue",255);
 
 
 		//Toolbar icons
-		loadImage(display,"lbms/azmultiuser/remote/client/firefrog/resources/open_by_file_new.png","open_by_file",255);
-		loadImage(display,"lbms/azmultiuser/remote/client/firefrog/resources/open_by_url_new.png","open_by_url",255);
-		loadImage(display,"lbms/azmultiuser/remote/client/firefrog/resources/preferences.png","preferences",255);
-		loadImage(display,"lbms/azmultiuser/remote/client/firefrog/resources/logout_new.png","logout",255);
-		loadImage(display,"lbms/azmultiuser/remote/client/firefrog/resources/information.png","information",255);
-		loadImage(display,"lbms/azmultiuser/remote/client/firefrog/resources/connect_new.png","connect",255);
-		loadImage(display,"lbms/azmultiuser/remote/client/firefrog/resources/connect_quick_new.png","connect_quick",255);
-		loadImage(display,"lbms/azmultiuser/remote/client/firefrog/resources/refresh.png","refresh",255);
-		loadImage(display,"lbms/azmultiuser/remote/client/firefrog/resources/tool_pause.png","pause",255);
-		loadImage(display,"lbms/azmultiuser/remote/client/firefrog/resources/tool_resume.png","resume",255);
-        //loadImage(display,"lbms/azmultiuser/remote/client/firefrog/resources/server.png","server",255);
-        loadImage(display,"lbms/azmultiuser/remote/client/firefrog/resources/manage_users.png","manager_users",255);
-        loadImage(display,"lbms/azmultiuser/remote/client/firefrog/resources/console.png","console",255);
+		loadImage(display,"lbms/azmultiuser/remote/client/swtgui/resources/open_by_file_new.png","open_by_file",255);
+		loadImage(display,"lbms/azmultiuser/remote/client/swtgui/resources/open_by_url_new.png","open_by_url",255);
+		loadImage(display,"lbms/azmultiuser/remote/client/swtgui/resources/preferences.png","preferences",255);
+		loadImage(display,"lbms/azmultiuser/remote/client/swtgui/resources/logout_new.png","logout",255);
+		loadImage(display,"lbms/azmultiuser/remote/client/swtgui/resources/information.png","information",255);
+		loadImage(display,"lbms/azmultiuser/remote/client/swtgui/resources/connect_new.png","connect",255);
+		loadImage(display,"lbms/azmultiuser/remote/client/swtgui/resources/connect_quick_new.png","connect_quick",255);
+		loadImage(display,"lbms/azmultiuser/remote/client/swtgui/resources/refresh.png","refresh",255);
+		loadImage(display,"lbms/azmultiuser/remote/client/swtgui/resources/tool_pause.png","pause",255);
+		loadImage(display,"lbms/azmultiuser/remote/client/swtgui/resources/tool_resume.png","resume",255);
+        //loadImage(display,"lbms/azmultiuser/remote/client/swtgui/resources/server.png","server",255);
+        loadImage(display,"lbms/azmultiuser/remote/client/swtgui/resources/manage_users.png","manager_users",255);
+        loadImage(display,"lbms/azmultiuser/remote/client/swtgui/resources/console.png","console",255);
 
-		loadImage(display,"lbms/azmultiuser/remote/client/firefrog/resources/popup.png","popup",255);
-        loadImage(display,"lbms/azmultiuser/remote/client/firefrog/resources/toolbar/queue_large.png","toolbar_queue",255);
-        loadImage(display,"lbms/azmultiuser/remote/client/firefrog/resources/toolbar/remove_large.png","toolbar_remove",255);
-        loadImage(display,"lbms/azmultiuser/remote/client/firefrog/resources/toolbar/stop_large.png","toolbar_stop",255);
+		loadImage(display,"lbms/azmultiuser/remote/client/swtgui/resources/popup.png","popup",255);
+        loadImage(display,"lbms/azmultiuser/remote/client/swtgui/resources/toolbar/queue_large.png","toolbar_queue",255);
+        loadImage(display,"lbms/azmultiuser/remote/client/swtgui/resources/toolbar/remove_large.png","toolbar_remove",255);
+        loadImage(display,"lbms/azmultiuser/remote/client/swtgui/resources/toolbar/stop_large.png","toolbar_stop",255);
 
         //paypal
-        loadImage(display,"lbms/azmultiuser/remote/client/firefrog/resources/paypal.gif","paypal",255);
+        loadImage(display,"lbms/azmultiuser/remote/client/swtgui/resources/paypal.gif","paypal",255);
 
 		//if needed
-		loadImage(display,"lbms/azmultiuser/remote/client/firefrog/resources/tool_delete.png","trashcan",255);
+		loadImage(display,"lbms/azmultiuser/remote/client/swtgui/resources/tool_delete.png","trashcan",255);
 
 
         //Server Details toolbar icons
-        loadImage(display,"lbms/azmultiuser/remote/client/firefrog/resources/serverIcons/user_add.png","add",255);
-        loadImage(display,"lbms/azmultiuser/remote/client/firefrog/resources/serverIcons/user_delete.png","delete",255);
-        loadImage(display,"lbms/azmultiuser/remote/client/firefrog/resources/serverIcons/folder_new.png","folder",255);
+        loadImage(display,"lbms/azmultiuser/remote/client/swtgui/resources/serverIcons/user_add.png","add",255);
+        loadImage(display,"lbms/azmultiuser/remote/client/swtgui/resources/serverIcons/user_delete.png","delete",255);
+        loadImage(display,"lbms/azmultiuser/remote/client/swtgui/resources/serverIcons/folder_new.png","folder",255);
 
 	}
 
