19c3261867885a30a9e18aeee11785d978caa2ab
damokles <damokles@58cabe8c-710f-0410-8395-bf4839b10e17>
2006-04-07 16:30:17 +0000

    fixed update url
    git-svn-id: http://svn.code.sf.net/p/azsmrc/code/trunk@155 58cabe8c-710f-0410-8395-bf4839b10e17

--- azsmrc/lbms/azsmrc/shared/RemoteConstants.java
+++ azsmrc/lbms/azsmrc/shared/RemoteConstants.java
@@ -91,7 +91,7 @@ public class RemoteConstants {
 
 
 	//Update URL
-	public static final String UPDATE_URL = "http://azcvsupdater.sourceforge.net/azsmrc/AzSMRCupdate.xml.gz";
+	public static final String UPDATE_URL = "http://azsmrc.sourceforge.net/AzSMRCupdate.xml.gz";
 
 	//Encoder stuff
 	public static final String DEFAULT_ENCODING 	= "UTF8";
