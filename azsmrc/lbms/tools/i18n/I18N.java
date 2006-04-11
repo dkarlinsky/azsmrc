4752ec6ae4d7011301fa1409503ce9b1f20fb3c0
damokles <damokles@58cabe8c-710f-0410-8395-bf4839b10e17>
2006-04-11 17:43:18 +0000

    I18N for Version 1.1
    git-svn-id: http://svn.code.sf.net/p/azsmrc/code/trunk@194 58cabe8c-710f-0410-8395-bf4839b10e17

---
+++ azsmrc/lbms/tools/i18n/I18N.java
@@ -0,0 +1,72 @@
+package lbms.tools.i18n;
+
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.HashMap;
+import java.util.Map;
+
+public class I18N {
+
+	private static Map<String, String> defaultMessages = new HashMap<String, String>();
+	private static Map<String, String> localizedMessages = new HashMap<String, String>();
+
+	private static boolean initialized, localized;
+
+	public static void initialize(File defaultFile) throws IOException {
+		load (new BufferedReader(new FileReader(defaultFile)), defaultMessages);
+		initialized = true;
+	}
+
+	public static void load (File localizedFile) throws IOException {
+		load (new BufferedReader(new FileReader(localizedFile)), localizedMessages);
+		localized = true;
+	}
+
+	private static void load (BufferedReader ir, Map<String,String> map) throws IOException {
+		try {
+			do {
+				String line = ir.readLine();
+				if (line == null) break; //EOF
+				if (line.indexOf("//") == 0) continue; //ignore comments
+				if (!line.contains("=")) continue; //invalid line
+				String[] parts = line.split("=", 2);
+				map.put(parts[0], parts[1]);
+			} while (true);
+
+		} finally {
+			if (ir!=null) ir.close();
+		}
+	}
+
+	public static String getMessage (String key) {
+		if (localizedMessages.containsKey(key))
+			return localizedMessages.get(key);
+		else
+			return getDefaultMessage(key);
+	}
+
+	public static String getDefaultMessage (String key) {
+		if (defaultMessages.containsKey(key))
+			return defaultMessages.get(key);
+		else
+			return key;
+	}
+
+	/**
+	 * @return Returns the initialized.
+	 */
+	public static boolean isInitialized() {
+		return initialized;
+	}
+
+	/**
+	 * @return Returns the localized.
+	 */
+	public static boolean isLocalized() {
+		return localized;
+	}
+
+
+}
