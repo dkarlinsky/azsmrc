a5be60bd190ce3ebfc95ac34cf24d4e3499f4990
damokles <damokles@58cabe8c-710f-0410-8395-bf4839b10e17>
2006-04-11 16:38:15 +0000

    Added Stats Streams
    git-svn-id: http://svn.code.sf.net/p/azsmrc/code/trunk@191 58cabe8c-710f-0410-8395-bf4839b10e17

---
+++ azsmrc/lbms/tools/stats/StatsInputStream.java
@@ -0,0 +1,77 @@
+package lbms.tools.stats;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.util.ArrayList;
+import java.util.List;
+
+public class StatsInputStream extends InputStream {
+
+	private long bytesRead		= 0;
+	private long lastTime		= 0;
+	private long lastBytesRead	= 0;
+	private long bytesPerSec	= 0;
+	private InputStream is;
+	private List<StatsStreamSpeedListener> listeners = new ArrayList<StatsStreamSpeedListener>();
+
+	private StatsInputStream() {
+		super();
+		// TODO Auto-generated constructor stub
+	}
+
+	public StatsInputStream(InputStream is) {
+		super();
+		this.is = is;
+		lastTime = System.currentTimeMillis();
+	}
+
+	@Override
+	public int read() throws IOException {
+		bytesRead++;
+		if ((bytesRead%1024)==0) {
+			long now = System.currentTimeMillis();
+			long diff = now-lastTime ;
+			if (diff>=1000) {
+				bytesPerSec = (long)((bytesRead-lastBytesRead)*(1000d/(diff)));
+				for (StatsStreamSpeedListener l:listeners) {
+					l.speedPerSec(bytesPerSec);
+				}
+				lastBytesRead = bytesRead;
+				lastTime = now;
+			}
+		}
+		return is.read();
+	}
+
+	@Override
+	public void close() throws IOException {
+		is.close();
+		super.close();
+	}
+
+	/**
+	 * @return Returns the bytesRead.
+	 */
+	public long getBytesRead() {
+		return bytesRead;
+	}
+
+	/**
+	 * @return Returns the bytesPerSec.
+	 */
+	public long getBytesPerSec() {
+		return bytesPerSec;
+	}
+
+	public void setTime() {
+		lastTime = System.currentTimeMillis();
+	}
+
+	public void addSpeedListener (StatsStreamSpeedListener l) {
+		listeners.add(l);
+	}
+
+	public void removeSpeedListener (StatsStreamSpeedListener l) {
+		listeners.remove(l);
+	}
+}
