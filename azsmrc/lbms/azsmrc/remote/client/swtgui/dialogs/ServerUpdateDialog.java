9d340e53c3efa3e792a1fb9fed5801c2366a7e6c
omschaub <omschaub@58cabe8c-710f-0410-8395-bf4839b10e17>
2006-04-13 18:07:45 +0000

    Start of the ServerUpdateDialog
    git-svn-id: http://svn.code.sf.net/p/azsmrc/code/trunk@215 58cabe8c-710f-0410-8395-bf4839b10e17

---
+++ azsmrc/lbms/azsmrc/remote/client/swtgui/dialogs/ServerUpdateDialog.java
@@ -0,0 +1,139 @@
+/*
+ * Created on Apr 13, 2006
+ * Created by omschaub
+ *
+ */
+package lbms.azsmrc.remote.client.swtgui.dialogs;
+
+import lbms.azsmrc.remote.client.RemoteUpdate;
+import lbms.azsmrc.remote.client.RemoteUpdateManager;
+import lbms.azsmrc.remote.client.swtgui.GUI_Utilities;
+import lbms.azsmrc.remote.client.swtgui.RCMain;
+
+import org.eclipse.swt.SWT;
+import org.eclipse.swt.layout.GridData;
+import org.eclipse.swt.layout.GridLayout;
+import org.eclipse.swt.widgets.Button;
+import org.eclipse.swt.widgets.Composite;
+import org.eclipse.swt.widgets.Event;
+import org.eclipse.swt.widgets.Label;
+import org.eclipse.swt.widgets.Listener;
+import org.eclipse.swt.widgets.Shell;
+import org.eclipse.swt.widgets.Table;
+import org.eclipse.swt.widgets.TableColumn;
+import org.eclipse.swt.widgets.TableItem;
+
+
+public class ServerUpdateDialog {
+
+
+
+	public ServerUpdateDialog(RemoteUpdateManager rum){
+
+		//Shell
+		final Shell shell = new Shell(RCMain.getRCMain().getDisplay());
+		shell.setLayout(new GridLayout(1,false));
+		shell.setText("Updates Available");
+
+		//Comp on shell
+		Composite comp = new Composite(shell,SWT.NULL);
+		GridData gridData = new GridData(GridData.GRAB_HORIZONTAL);
+		comp.setLayoutData(gridData);
+
+		GridLayout gridLayout = new GridLayout();
+		gridLayout.numColumns = 2;
+		gridLayout.marginWidth = 2;
+		comp.setLayout(gridLayout);
+		
+
+		//first line
+		Label infoLabel = new Label(comp,SWT.BORDER | SWT.CENTER);
+		infoLabel.setText("Azureus Updates Available\nServer: " + RCMain.getRCMain().getClient().getServer().getHost() + "\nNumber of Updates: "+ rum.getUpdates().length);
+		infoLabel.setBackground(RCMain.getRCMain().getDisplay().getSystemColor(SWT.COLOR_GRAY));		
+		
+		GridData gd = new GridData(GridData.FILL_HORIZONTAL);
+		gd.horizontalSpan = 2;
+		infoLabel.setLayoutData(gd);
+
+		//Table for updates
+		final Table table = new Table(comp,SWT.V_SCROLL | SWT.CHECK | SWT.SINGLE);
+		gd = new GridData(GridData.FILL_BOTH);
+		gd.horizontalSpan = 2;
+		gd.verticalSpan = 30;
+		table.setLayoutData(gd);
+		table.setHeaderVisible(true);
+		
+		TableColumn spacerForCheck = new TableColumn(table,SWT.NULL);
+		spacerForCheck.setWidth(30);
+		
+		TableColumn name = new TableColumn(table,SWT.NULL);
+		name.setText("Name");
+		name.setWidth(300);
+		
+		TableColumn version = new TableColumn(table,SWT.NULL);
+		version.setText("New Version");
+		version.setWidth(100);
+		
+		//populate the table with the updates
+		RemoteUpdate[] rus = rum.getUpdates();
+		for(RemoteUpdate ru:rus){
+			TableItem item = new TableItem(table,SWT.NULL);
+			item.setText(1,ru.getName());
+			item.setText(ru.getNewVersion());
+			item.setChecked(true);
+			item.setData(ru);
+		}
+		
+		
+		
+		
+
+		//Bottom Buttons		
+		Composite button_comp = new Composite(shell, SWT.NULL);
+		gridData = new GridData(GridData.FILL_HORIZONTAL);
+		gridData.horizontalSpan = 2;
+		button_comp.setLayoutData(gridData);
+
+		gridLayout = new GridLayout();
+		gridLayout.numColumns = 3;
+		gridLayout.marginWidth = 0;
+		button_comp.setLayout(gridLayout);
+
+
+		Button commit = new Button(button_comp,SWT.PUSH);
+		commit.setText("Send Checked to Server");
+		gd = new GridData(GridData.HORIZONTAL_ALIGN_BEGINNING);
+		commit.setLayoutData(gd);
+		commit.addListener(SWT.Selection, new Listener(){
+			public void handleEvent(Event arg0) {				
+				TableItem[] items = table.getItems();				
+				for(TableItem item:items){					
+					if(item.getChecked()){
+						RemoteUpdate ru = (RemoteUpdate)item.getData();
+						//do something with the ru here
+					}
+				}
+			}			
+		});
+		
+		
+		
+		
+		Button cancel = new Button(button_comp,SWT.PUSH);
+		cancel.setText("Cancel");
+		gd = new GridData(GridData.HORIZONTAL_ALIGN_END);
+		gd.grabExcessHorizontalSpace = true;
+		gd.horizontalSpan = 2;
+		cancel.setLayoutData(gd);
+		cancel.addListener(SWT.Selection, new Listener(){
+			public void handleEvent(Event e) {
+				shell.close();
+			 }
+		 });
+
+
+
+		//Center and open shell
+		GUI_Utilities.centerShellandOpen(shell);
+	}
+}
