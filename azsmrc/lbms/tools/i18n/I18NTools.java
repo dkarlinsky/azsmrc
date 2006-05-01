4419cda7baf956a13f37a0a1e867d0a5f1542b1c
damokles <damokles@58cabe8c-710f-0410-8395-bf4839b10e17>
2006-05-01 12:55:32 +0000

    added JavaDoc
    git-svn-id: http://svn.code.sf.net/p/azsmrc/code/trunk@341 58cabe8c-710f-0410-8395-bf4839b10e17

--- azsmrc/lbms/tools/i18n/I18NTools.java
+++ azsmrc/lbms/tools/i18n/I18NTools.java
@@ -12,6 +12,16 @@ import java.util.TreeSet;
 
 public class I18NTools {
 
+	/**
+	 * Writes the Map to the file.
+	 * 
+	 * The Map is written to the file
+	 * key=value
+	 * 
+	 * @param file
+	 * @param map
+	 * @throws IOException
+	 */
 	public static void writeToFile (File file,Map<String, String> map) throws IOException {
 		FileWriter fw = null;
 		try {
@@ -25,12 +35,28 @@ public class I18NTools {
 		}
 	}
 
+	/**
+	 * Reads a Map from a File
+	 * 
+	 * @param file
+	 * @return
+	 * @throws IOException
+	 */
 	public static Map<String,String> readFromFile (File file) throws IOException {
 		Map<String, String> map = new TreeMap<String, String>();
 		I18NTranslator.load(new BufferedReader(new FileReader(file)), map);
 		return map;
 	}
 
+	/**
+	 * This duplicates a Map.
+	 * 
+	 * All Keys are duplicated and the values
+	 * are initialized with ""
+	 * 
+	 * @param map
+	 * @return
+	 */
 	public static Map<String,String> duplicate (Map<String,String> map) {
 		Map<String, String> target = new TreeMap<String, String>();
 		SortedSet<String> keys = new TreeSet<String> (map.keySet()); //sort all entries
@@ -40,6 +66,15 @@ public class I18NTools {
 		return target;
 	}
 
+	/**
+	 * Merges two Maps
+	 * 
+	 * This will add all keys to then target
+	 * that src has and target don't
+	 * 
+	 * @param src
+	 * @param target
+	 */
 	public static void merge (Map<String,String> src, Map<String,String> target) {
 		SortedSet<String> keys = new TreeSet<String> (src.keySet());
 		for (String key:keys) {
