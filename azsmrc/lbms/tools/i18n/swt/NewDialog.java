7efd4cb59b745fac2afac16bb1f121be96426073
omschaub <omschaub@58cabe8c-710f-0410-8395-bf4839b10e17>
2006-05-02 14:07:15 +0000

    Adding a dialog for adding in a new key
    git-svn-id: http://svn.code.sf.net/p/azsmrc/code/trunk@359 58cabe8c-710f-0410-8395-bf4839b10e17

---
+++ azsmrc/lbms/tools/i18n/swt/NewDialog.java
@@ -0,0 +1,146 @@
+package lbms.tools.i18n.swt;
+
+import org.eclipse.swt.SWT;
+import org.eclipse.swt.layout.GridData;
+import org.eclipse.swt.layout.GridLayout;
+import org.eclipse.swt.widgets.Button;
+import org.eclipse.swt.widgets.Composite;
+import org.eclipse.swt.widgets.Display;
+import org.eclipse.swt.widgets.Event;
+import org.eclipse.swt.widgets.Label;
+import org.eclipse.swt.widgets.Listener;
+import org.eclipse.swt.widgets.Shell;
+import org.eclipse.swt.widgets.Text;
+
+
+public class NewDialog {	
+	private String sTitleKey;
+	
+	
+	
+	private String[] textValue = {"",""};
+
+
+	public NewDialog(String Title) {
+		this.sTitleKey = Title;
+		
+		
+		
+
+		this.setTextValue("","");
+	}
+
+	public String[] open() {
+		final Display display = I18NMe.getI18NMe().getDisplay();
+		if (display == null)
+			return null;
+
+		final Shell shell = new Shell(display.getActiveShell());
+		shell.setText(sTitleKey);
+		
+
+		GridLayout layout = new GridLayout();
+		shell.setLayout(layout);
+
+		Label label = new Label(shell, SWT.WRAP);
+		label.setText("New Key Name");
+		
+		GridData gridData = new GridData(GridData.FILL_HORIZONTAL);
+		gridData.widthHint = 200;
+		label.setLayoutData(gridData);
+
+		final Text textKey = new Text(shell, SWT.BORDER);
+		gridData = new GridData();
+		gridData.widthHint = 300;
+		textKey.setLayoutData(gridData);
+		textKey.setText(textValue[0]);
+		textKey.selectAll();
+
+		
+		Label label2 = new Label(shell, SWT.WRAP);
+		label2.setText("Default Value");
+		
+		gridData = new GridData(GridData.FILL_HORIZONTAL);
+		gridData.widthHint = 200;
+		label.setLayoutData(gridData);
+
+		final Text textObj = new Text(shell, SWT.BORDER);
+		gridData = new GridData();
+		gridData.widthHint = 300;
+		textObj.setLayoutData(gridData);
+		textObj.setText(textValue[1]);
+		textObj.selectAll();
+		
+		
+		Composite panel = new Composite(shell, SWT.NULL);
+		layout = new GridLayout();
+		layout.numColumns = 3;
+		panel.setLayout(layout);
+		gridData = new GridData();
+		gridData.horizontalSpan = 2;
+		panel.setLayoutData(gridData);
+		Button ok = new Button(panel, SWT.PUSH);
+		ok.setText("OK");
+		gridData = new GridData();
+		gridData.widthHint = 70;
+		ok.setLayoutData(gridData);
+		shell.setDefaultButton(ok);
+		ok.addListener(SWT.Selection, new Listener() {
+			/* (non-Javadoc)
+			 * @see org.eclipse.swt.widgets.Listener#handleEvent(org.eclipse.swt.widgets.Event)
+			 */
+			public void handleEvent(Event event) {
+				try {
+					setTextValue(textKey.getText(), textObj.getText());
+					shell.dispose();
+				} catch (Exception e) {
+					e.printStackTrace();
+				}
+			}
+		});
+
+		Button cancel = new Button(panel, SWT.PUSH);
+		cancel.setText("Cancel");
+		gridData = new GridData();
+		gridData.widthHint = 70;
+		cancel.setLayoutData(gridData);
+		cancel.addListener(SWT.Selection, new Listener() {
+			/* (non-Javadoc)
+			 * @see org.eclipse.swt.widgets.Listener#handleEvent(org.eclipse.swt.widgets.Event)
+			 */
+			public void handleEvent(Event event) {
+				shell.dispose();
+			}
+		});
+
+		shell.pack();
+		MainWindow.centerShellandOpen(shell);		
+		setTextValue(null,null);
+		shell.open();
+
+		while (!shell.isDisposed())
+			if (!display.readAndDispatch())
+				display.sleep();
+
+		return getTextValue();
+	}
+
+	/**
+	 * @param textValue The textValue to set.
+	 */
+	public void setTextValue(String keyValue, String defaultValue) {
+		this.textValue[0] = keyValue;
+		this.textValue[1] = defaultValue;
+	}
+
+	/**
+	 * @return Returns the textValue.
+	 */
+	public String[] getTextValue() {
+		return textValue;
+	}
+
+
+}
+
+
