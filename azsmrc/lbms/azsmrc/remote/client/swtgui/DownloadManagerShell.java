8a0cfb8a164d63766c101d6fc6ca4c030ab2db2b
omschaub <omschaub@58cabe8c-710f-0410-8395-bf4839b10e17>
2006-05-31 15:30:22 +0000

    Removing the import for the ErrorDialog
    git-svn-id: http://svn.code.sf.net/p/azsmrc/code/trunk@507 58cabe8c-710f-0410-8395-bf4839b10e17

--- azsmrc/lbms/azsmrc/remote/client/swtgui/DownloadManagerShell.java
+++ azsmrc/lbms/azsmrc/remote/client/swtgui/DownloadManagerShell.java
@@ -39,7 +39,6 @@ import lbms.azsmrc.remote.client.swtgui.container.Container;
 import lbms.azsmrc.remote.client.swtgui.container.DownloadContainer;
 import lbms.azsmrc.remote.client.swtgui.container.SeedContainer;
 import lbms.azsmrc.remote.client.swtgui.dialogs.ConnectionDialog;
-import lbms.azsmrc.remote.client.swtgui.dialogs.ErrorDialog;
 import lbms.azsmrc.remote.client.swtgui.dialogs.InputShell;
 import lbms.azsmrc.remote.client.swtgui.dialogs.MoveDataDialog;
 import lbms.azsmrc.remote.client.swtgui.dialogs.OpenByFileDialog;
