92d545ac7dad675546337ba91bbb9961af5a1d23
omschaub <omschaub@58cabe8c-710f-0410-8395-bf4839b10e17>
2006-09-08 15:04:15 +0000

    Icon for dialog
    git-svn-id: http://svn.code.sf.net/p/azsmrc/code/trunk@1015 58cabe8c-710f-0410-8395-bf4839b10e17

--- azsmrc/lbms/azsmrc/remote/client/swtgui/dialogs/ScrapeDialog.java
+++ azsmrc/lbms/azsmrc/remote/client/swtgui/dialogs/ScrapeDialog.java
@@ -78,7 +78,8 @@ public class ScrapeDialog {
 		shell.setLayout(new GridLayout(1,false));
 		shell.setText(I18N.translate(PFX + "shell.text"));
 
-
+		if(!lbms.azsmrc.remote.client.Utilities.isOSX)
+			shell.setImage(ImageRepository.getImage("TrayIcon_Blue"));
 
 		tabFolder = new CTabFolder(shell, SWT.FLAT);
 		GridData gd = new GridData(GridData.FILL_BOTH);
