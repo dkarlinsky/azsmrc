a97929185c11583100d62f17d09c10993b842df5
tuebernickel <tuebernickel@58cabe8c-710f-0410-8395-bf4839b10e17>
2006-11-23 03:08:43 +0000

    fileUpload.js 	correct base64 encoding, torrentfileupload works
    git-svn-id: http://svn.code.sf.net/p/azsmrc/code/trunk@1181 58cabe8c-710f-0410-8395-bf4839b10e17

--- azsmrc/lbms/azsmrc/plugin/web/resources/js/fileUpload.js
+++ azsmrc/lbms/azsmrc/plugin/web/resources/js/fileUpload.js
@@ -1,4 +1,3 @@
-var binary;
 function addUploadContent() {
 	var div = document.createElement("div");
 	var head = document.createElement("h2");
@@ -27,7 +26,32 @@ function addUploadContent() {
 	div.appendChild(form);
 	return div;
 }
+function encode64(input) {
+	var keyStr = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789+/=";
+	var output = "";
+	var chr1, chr2, chr3, enc1, enc2, enc3, enc4;
+	var i = 0;
+	while (i < input.length) {
+		chr1 = input.charCodeAt(i++);
+		chr2 = input.charCodeAt(i++);
+		chr3 = input.charCodeAt(i++);
+
+		enc1 = chr1 >> 2;
+		enc2 = ((chr1 & 3) << 4) | (chr2 >> 4);
+		enc3 = ((chr2 & 15) << 2) | (chr3 >> 6);
+		enc4 = chr3 & 63;
+
+		if (isNaN(chr2)) {
+			enc3 = enc4 = 64;
+		} else if (isNaN(chr3)) {
+			enc4 = 64;
+		}
+		output = output + keyStr.charAt(enc1) + keyStr.charAt(enc2) + keyStr.charAt(enc3) + keyStr.charAt(enc4);
+	}
+	return output;
+}
 function uploadFile() {
+	var binary;
 	var filename = document.getElementById("torrentfile").value;
 	// request more permissions
 	try {
@@ -50,9 +74,8 @@ function uploadFile() {
 	var requestURL = Server+"process.cgi";
 	var xmlhttp = createXMLHTTP();	
 	if (xmlhttp) {
-		var statusbarentry = document.getElementById("requeststatus").firstChild;
-		var filecontent = escape(binary.readBytes(binary.available()));
-		var boundaryString = 'base64';
+		var statusbarentry = document.getElementById("requeststatus").firstChild;		
+		var filecontent = encode64(binary.readBytes(binary.available()));
 		var xmlrequest = '<?xml version="1.0" encoding="UTF-8"?><Request version="1.0">'
 			+ '<Query switch="addDownload" location="xml">'
 			+ '<Torrent>'+ filecontent +'</Torrent>'
@@ -83,7 +106,7 @@ function uploadFile() {
 			}
 		}
 		xmlhttp.open("POST", requestURL, true);
-		xmlhttp.setRequestHeader("Content-type", "application/xml; boundary=\"" + boundaryString + "\"");
+		xmlhttp.setRequestHeader("Content-type", "application/xml;");
 		xmlhttp.setRequestHeader("Connection", "close");
 		xmlhttp.setRequestHeader("Content-length", xmlrequest.length);
 		xmlhttp.send(xmlrequest);
