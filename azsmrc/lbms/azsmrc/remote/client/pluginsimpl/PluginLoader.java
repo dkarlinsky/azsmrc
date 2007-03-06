08ced1163ece6b1a264933d8e042c577ced45226
Damokles <Damokles@58cabe8c-710f-0410-8395-bf4839b10e17>
2007-03-06 19:29:15 +0000

    fixed variable swap
    git-svn-id: http://svn.code.sf.net/p/azsmrc/code/trunk@1367 58cabe8c-710f-0410-8395-bf4839b10e17

--- azsmrc/lbms/azsmrc/remote/client/pluginsimpl/PluginLoader.java
+++ azsmrc/lbms/azsmrc/remote/client/pluginsimpl/PluginLoader.java
@@ -130,7 +130,7 @@ public class PluginLoader {
 					//if we don't want to load this plugin continue
 					if (!azsmrcProps.getPropertyAsBoolean("plugins."+pluginID+".load")) {
 						System.out.println( "Skipping load of "+pluginID);
-						manager.addDisabledPlugin(props, pluginID);
+						manager.addDisabledPlugin(props, plugin_dir.getCanonicalPath());
 						continue;
 					}
 				} else {
