64f8cf3da034c518163c178ce532c5ca27717e67
omschaub <omschaub@58cabe8c-710f-0410-8395-bf4839b10e17>
2007-05-22 01:39:42 +0000

    A little more on the torrent creation dialog, just to show Leonard I worked some today ^^
    git-svn-id: http://svn.code.sf.net/p/azsmrc/code/trunk@1455 58cabe8c-710f-0410-8395-bf4839b10e17

--- azsmrc/lbms/azsmrc/remote/client/swtgui/dialogs/CreateTorrentDialog.java
+++ azsmrc/lbms/azsmrc/remote/client/swtgui/dialogs/CreateTorrentDialog.java
@@ -44,7 +44,8 @@ public class CreateTorrentDialog {
 	private Display display;
 	private StackLayout sLayout;
 	private int pageNum = 0;
-	
+	private Label fileDirLabel;
+
 	//Settings
 	private int trackerInt;
 	private String customTracker;
@@ -52,13 +53,14 @@ public class CreateTorrentDialog {
 	private boolean boolOtherHashes = false;
 	private boolean boolFile = true;
 	private String comment = "";
-	
-	
+	private String fileDir = "";
+
+
 	private CreateTorrentDialog(final String url){
 		instance = this;
 		display = RCMain.getRCMain().getDisplay();
 		if(display == null || display.isDisposed()) return;
-		
+
 		//Shell
 		shell = new Shell(display);
 		final GridLayout layout = new GridLayout(1,false);
@@ -83,15 +85,15 @@ public class CreateTorrentDialog {
 
 		// create the first page's content
 		createPage0(parentComp);
-		// create the second page's content	
+		// create the second page's content
 		createPage1(parentComp);
-		
+
 
 		sLayout.topControl = page0;
 		parentComp.layout ();
 
 
-		
+
 		//------------------  BUTTON COMP --------------------------\\
 
 		Composite button_comp = new Composite(shell, SWT.NULL);
@@ -100,7 +102,7 @@ public class CreateTorrentDialog {
 
 		GridLayout gridLayout = new GridLayout();
 		gridLayout.numColumns = 4;
-		
+
 		button_comp.setLayout(gridLayout);
 
 		bCancel = new Button(button_comp,SWT.PUSH);
@@ -110,7 +112,7 @@ public class CreateTorrentDialog {
 		gridData.widthHint = 75;
 		bCancel.setLayoutData(gridData);
 		bCancel.addListener(SWT.Selection, new Listener(){
-			public void handleEvent(Event e) {				
+			public void handleEvent(Event e) {
 				shell.close();
 			}
 		});
@@ -128,7 +130,7 @@ public class CreateTorrentDialog {
 				sLayout.topControl = pageNum == 0 ? page0 : page1;
 				parentComp.layout ();
 				bPrevious.setEnabled(false);
-				bNext.setEnabled(true);	
+				bNext.setEnabled(true);
 			}
 		});
 		bPrevious.setEnabled(false);
@@ -145,10 +147,10 @@ public class CreateTorrentDialog {
 				sLayout.topControl = pageNum == 0 ? page0 : page1;
 				parentComp.layout ();
 				bPrevious.setEnabled(true);
-				bNext.setEnabled(false);				
+				bNext.setEnabled(false);
 			}
 		});
-		
+
 		bFinish = new Button(button_comp,SWT.PUSH);
 		gridData = new GridData(GridData.HORIZONTAL_ALIGN_END);
 		gridData.widthHint = 75;
@@ -160,22 +162,22 @@ public class CreateTorrentDialog {
 			}
 		});
 		bFinish.setEnabled(false);
-		
-		
+
+
 		//Center Shell and open
 		GUI_Utilities.centerShellOpenAndFocus(shell);
 	}
 
-	
+
 	/**
 	 * Create contents for page0 (first page)
 	 * @param parentComp
 	 */
-	private void createPage0(Composite parentComp){		
+	private void createPage0(Composite parentComp){
 		page0 = new Composite (parentComp, SWT.BORDER);
 		GridData gridData = new GridData(GridData.GRAB_HORIZONTAL);
 		page0.setLayoutData(gridData);
-		
+
 
 		GridLayout gridLayout = new GridLayout();
 		gridLayout.marginWidth = 0;
@@ -184,83 +186,83 @@ public class CreateTorrentDialog {
 		gridLayout.verticalSpacing = 0;
 		gridLayout.horizontalSpacing = 0;
 		page0.setLayout(gridLayout);
-		
+
 		//two composits.. top white one and bottom settings one
-		
+
 		//top white
 		Composite topWhiteComp = new Composite(page0,SWT.BORDER);
 		topWhiteComp.setLayoutData(new GridData(GridData.FILL_HORIZONTAL));
 		topWhiteComp.setLayout(new GridLayout(1,false));
 		topWhiteComp.setBackground(display.getSystemColor(SWT.COLOR_WHITE));
-		
+
 
 		Label label = new Label (topWhiteComp, SWT.NONE);
 		label.setText(I18N.translate(PFX + "page0.topWhiteComp.label.text"));
 		label.setBackground(display.getSystemColor(SWT.COLOR_WHITE));
-		
-		
+
+
 		//Settings Comp
 		Composite settingsComp = new Composite(page0, SWT.BORDER);
 		settingsComp.setLayoutData(new GridData(GridData.FILL_BOTH));
 		settingsComp.setLayout(new GridLayout(1,false));
-		
+
 		Button radio0 = new Button(settingsComp, SWT.RADIO);
 		radio0.setText(I18N.translate(PFX + "page0.settings.tracker0.text"));
-	
+
 		Button radio1 = new Button(settingsComp, SWT.RADIO);
 		radio1.setText(I18N.translate(PFX + "page0.settings.tracker1.text"));
-		
+
 		final Composite customTrackerComp = new Composite(settingsComp, SWT.NULL);
 		customTrackerComp.setLayoutData(new GridData(GridData.FILL_HORIZONTAL));
-		customTrackerComp.setLayout(new GridLayout(2,false));		
-		
+		customTrackerComp.setLayout(new GridLayout(2,false));
+
 		final Label customTrackerLabel = new Label(customTrackerComp,SWT.NULL);
 		customTrackerLabel.setLayoutData(new GridData(GridData.HORIZONTAL_ALIGN_BEGINNING));
 		customTrackerLabel.setText(I18N.translate(PFX + "page0.settings.customannounceURL.label"));
-		
+
 		final Text customTrackerText = new Text(customTrackerComp, SWT.BORDER);
 		customTrackerText.setLayoutData(new GridData(GridData.FILL_HORIZONTAL));
 		customTrackerText.setText("http://");
 		customTrackerText.addModifyListener(new ModifyListener(){
 
 			public void modifyText(ModifyEvent arg0) {
-				customTracker = customTrackerText.getText();				
-			}			
+				customTracker = customTrackerText.getText();
+			}
 		});
-		
-		
+
+
 		Button radio2 = new Button(settingsComp, SWT.RADIO);
 		radio2.setText(I18N.translate(PFX + "page0.settings.tracker2.text"));
-		
-		
+
+
 		//Listeners for radiobuttons
 		radio0.addListener(SWT.Selection, new Listener(){
-			public void handleEvent(Event arg0) {			
+			public void handleEvent(Event arg0) {
 				trackerInt = 0;
 				customTrackerLabel.setEnabled(false);
 				customTrackerText.setEnabled(false);
-			}			
+			}
 		});
-		
-		
+
+
 		radio1.addListener(SWT.Selection, new Listener(){
-			public void handleEvent(Event arg0) {			
+			public void handleEvent(Event arg0) {
 				trackerInt = 1;
 				customTrackerLabel.setEnabled(true);
 				customTrackerText.setEnabled(true);
-			}			
+			}
 		});
 
-		
-		
+
+
 		radio2.addListener(SWT.Selection, new Listener(){
-			public void handleEvent(Event arg0) {			
+			public void handleEvent(Event arg0) {
 				trackerInt = 2;
 				customTrackerLabel.setEnabled(false);
 				customTrackerText.setEnabled(false);
-			}			
+			}
 		});
-		
+
 		switch(trackerInt){
 		case 0:
 			radio0.setSelection(true);
@@ -278,21 +280,21 @@ public class CreateTorrentDialog {
 			customTrackerLabel.setEnabled(false);
 			customTrackerText.setEnabled(false);
 			break;
-			
+
 		}
-		
-		
+
+
 		//Separator
 		Label separatorLabel1 = new Label(settingsComp, SWT.SEPARATOR | SWT.HORIZONTAL);
 		separatorLabel1.setLayoutData(new GridData(GridData.FILL_HORIZONTAL));
-		
-		
+
+
 		//Mode check boxes
 		//Depends on:
 		//private boolean boolMultiTracker
 		//private boolean boolOtherHashes
-		
-		
+
+
 		final Button bMode0 = new Button(settingsComp, SWT.CHECK);
 		bMode0.setText(I18N.translate(PFX + "page0.settings.mode0.text"));
 		bMode0.setLayoutData(new GridData(GridData.FILL_HORIZONTAL));
@@ -300,9 +302,9 @@ public class CreateTorrentDialog {
 		bMode0.addListener(SWT.Selection,new Listener(){
 			public void handleEvent(Event arg0) {
 				boolMultiTracker = bMode0.getSelection();
-			}			
+			}
 		});
-		
+
 		Button bMode1 = new Button(settingsComp, SWT.CHECK);
 		bMode1.setText(I18N.translate(PFX + "page0.settings.mode1.text"));
 		bMode1.setLayoutData(new GridData(GridData.FILL_HORIZONTAL));
@@ -310,67 +312,36 @@ public class CreateTorrentDialog {
 		bMode1.addListener(SWT.Selection,new Listener(){
 			public void handleEvent(Event arg0) {
 				boolOtherHashes = bMode0.getSelection();
-			}			
-		});		
-		
-		
+			}
+		});
+
+
 		//Separator
 		Label separatorLabel2 = new Label(settingsComp, SWT.SEPARATOR | SWT.HORIZONTAL);
 		separatorLabel2.setLayoutData(new GridData(GridData.FILL_HORIZONTAL));
-		
-		
-		Composite fileDirComp = new Composite(settingsComp, SWT.NULL);
-		fileDirComp.setLayoutData(new GridData(GridData.FILL_HORIZONTAL));
-		fileDirComp.setLayout(new GridLayout(2,true));
-		
-		Button bFile = new Button(fileDirComp, SWT.RADIO);
-		bFile.setText(I18N.translate(PFX + "page0.fileButton.text"));
-		gridData = new GridData(GridData.HORIZONTAL_ALIGN_CENTER);
-		gridData.grabExcessHorizontalSpace = true;
-		bFile.setLayoutData(gridData);
-		bFile.addListener(SWT.Selection, new Listener(){
-			public void handleEvent(Event arg0) {
-				boolFile = true;				
-			}			
-		});
-		
-		Button bDir = new Button(fileDirComp, SWT.RADIO);
-		bDir.setText(I18N.translate(PFX + "page0.dirButton.text"));
-		bDir.setLayoutData(new GridData(GridData.HORIZONTAL_ALIGN_CENTER));
-		bDir.addListener(SWT.Selection, new Listener(){
-			public void handleEvent(Event arg0) {
-				boolFile = false;				
-			}			
-		});		
-		
-		if(boolFile)
-			bFile.setSelection(true);
-		else
-			bDir.setSelection(false);
-		
-		//Separator
-		Label separatorLabel3 = new Label(settingsComp, SWT.SEPARATOR | SWT.HORIZONTAL);
-		separatorLabel3.setLayoutData(new GridData(GridData.FILL_HORIZONTAL));
-		
-		
+
+
+
+
+
 		//Comment Section
-		
+
 		final Label commentLabel = new Label(settingsComp,SWT.NULL);
 		commentLabel.setLayoutData(new GridData(GridData.HORIZONTAL_ALIGN_BEGINNING));
 		commentLabel.setText(I18N.translate(PFX + "page0.settings.comment.label"));
-		
+
 		final Text commentText = new Text(settingsComp, SWT.BORDER);
-		commentText.setLayoutData(new GridData(GridData.FILL_HORIZONTAL));	
+		commentText.setLayoutData(new GridData(GridData.FILL_HORIZONTAL));
 		commentText.setText(comment);
 		commentText.addModifyListener(new ModifyListener(){
 
 			public void modifyText(ModifyEvent arg0) {
-				comment = customTrackerText.getText();				
-			}			
+				comment = customTrackerText.getText();
+			}
 		});
 	}
 
-	
+
 	/**
 	 * Create contents for page1 (second page)
 	 * @param parentComp
@@ -379,7 +350,7 @@ public class CreateTorrentDialog {
 		page1 = new Composite (parentComp, SWT.BORDER);
 		GridData gridData = new GridData(GridData.GRAB_HORIZONTAL);
 		page1.setLayoutData(gridData);
-		
+
 
 		GridLayout gridLayout = new GridLayout();
 		gridLayout.marginWidth = 0;
@@ -388,22 +359,99 @@ public class CreateTorrentDialog {
 		gridLayout.verticalSpacing = 0;
 		gridLayout.horizontalSpacing = 0;
 		page1.setLayout(gridLayout);
-		
+
 		//two composits.. top white one and bottom settings one
-		
+
 		//top white
 		Composite topWhiteComp = new Composite(page1,SWT.BORDER);
 		topWhiteComp.setLayoutData(new GridData(GridData.FILL_HORIZONTAL));
 		topWhiteComp.setLayout(new GridLayout(1,false));
 		topWhiteComp.setBackground(display.getSystemColor(SWT.COLOR_WHITE));
-		
+
 
 		Label label = new Label (topWhiteComp, SWT.NONE);
 		label.setText(I18N.translate(PFX + "page1.topWhiteComp.label.text"));
 		label.setBackground(display.getSystemColor(SWT.COLOR_WHITE));
+
+		final Composite buttonComp = new Composite(page1, SWT.NULL);
+		buttonComp.setLayoutData(new GridData(GridData.FILL_HORIZONTAL));
+		buttonComp.setLayout(new GridLayout(2,true));
+
+		final Composite fileDirComp = new Composite(page1, SWT.NULL);
+		fileDirComp.setLayoutData(new GridData(GridData.FILL_BOTH));
+		fileDirComp.setLayout(new GridLayout(3,true));
+
+
+
+
+
+
+		Button bFile = new Button(buttonComp, SWT.RADIO);
+		bFile.setText(I18N.translate(PFX + "page0.fileButton.text"));
+		gridData = new GridData(GridData.HORIZONTAL_ALIGN_CENTER);
+		gridData.grabExcessHorizontalSpace = true;
+		bFile.setLayoutData(gridData);
+		bFile.addListener(SWT.Selection, new Listener(){
+			public void handleEvent(Event arg0) {
+				boolFile = true;
+				fileDirLabel.setText(I18N.translate(PFX + "page1.fileDirLabel.file.text"));
+				fileDirComp.layout();
+			}
+		});
+
+		Button bDir = new Button(buttonComp, SWT.RADIO);
+		bDir.setText(I18N.translate(PFX + "page0.dirButton.text"));
+		bDir.setLayoutData(new GridData(GridData.HORIZONTAL_ALIGN_CENTER));
+		bDir.addListener(SWT.Selection, new Listener(){
+			public void handleEvent(Event arg0) {
+				boolFile = false;
+				fileDirLabel.setText(I18N.translate(PFX + "page1.fileDirLabel.dir.text"));
+				fileDirComp.layout();
+			}
+		});
+
+		if(boolFile)
+			bFile.setSelection(true);
+		else
+			bDir.setSelection(false);
+
+		//Separator
+		Label separatorLabel3 = new Label(buttonComp, SWT.SEPARATOR | SWT.HORIZONTAL);
+		GridData gd = new GridData(GridData.FILL_HORIZONTAL);
+		gd.horizontalSpan = 2;
+		separatorLabel3.setLayoutData(gd);
+
+
+
+
+		//Label
+		fileDirLabel = new Label(fileDirComp, SWT.NULL);
+		fileDirLabel.setLayoutData(new GridData(GridData.HORIZONTAL_ALIGN_BEGINNING));
+		if(boolFile)
+			fileDirLabel.setText(I18N.translate(PFX + "page1.fileDirLabel.file.text"));
+		else
+			fileDirLabel.setText(I18N.translate(PFX + "page1.fileDirLabel.dir.text"));
+
+		//TextBox
+		final Text fileDirText = new Text(fileDirComp,SWT.BORDER);
+		fileDirText.setLayoutData(new GridData(GridData.FILL_HORIZONTAL));
+		fileDirText.addListener(SWT.Modify, new Listener(){
+			public void handleEvent(Event arg0) {
+				if(!fileDirText.getText().equalsIgnoreCase(""))
+					bFinish.setEnabled(true);
+				else
+					bFinish.setEnabled(false);
+			}
+		});
+
+
+		Button chooseFileDir = new Button(fileDirComp, SWT.BORDER);
+		chooseFileDir.setLayoutData(new GridData(GridData.HORIZONTAL_ALIGN_END));
+		
+		
 	}
-	
-	
+
+
 	/**
 	 * Open createTorrent dialog
 	 *
@@ -416,16 +464,16 @@ public class CreateTorrentDialog {
 			public void runSafe() {
 				if(display == null) return;
 				if (instance == null || instance.shell == null || instance.shell.isDisposed()){
-					new CreateTorrentDialog(url);					
+					new CreateTorrentDialog(url);
 				}else
 					instance.shell.setActive();
 
 			}
 		});
 	}
-	
-	
-	
+
+
+
 
 
 }//EOF
