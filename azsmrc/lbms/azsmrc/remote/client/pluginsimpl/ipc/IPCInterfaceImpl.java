ca1a911768afaca1b631a7025e94411278527d08
Damokles <Damokles@58cabe8c-710f-0410-8395-bf4839b10e17>
2007-09-27 00:12:39 +0000

    make target a WeakReference thus not stopping it from GC
    git-svn-id: http://svn.code.sf.net/p/azsmrc/code/trunk@1499 58cabe8c-710f-0410-8395-bf4839b10e17

--- azsmrc/lbms/azsmrc/remote/client/pluginsimpl/ipc/IPCInterfaceImpl.java
+++ azsmrc/lbms/azsmrc/remote/client/pluginsimpl/ipc/IPCInterfaceImpl.java
@@ -1,5 +1,6 @@
 package lbms.azsmrc.remote.client.pluginsimpl.ipc;
 
+import java.lang.ref.WeakReference;
 import java.lang.reflect.Method;
 
 import lbms.azsmrc.remote.client.plugins.ipc.IPCException;
@@ -7,24 +8,33 @@ import lbms.azsmrc.remote.client.plugins.ipc.IPCInterface;
 
 /**
  * @author Damokles
- *
+ * 
  */
 
 public class IPCInterfaceImpl implements IPCInterface {
 
-	Object target;
+	WeakReference<Object>	reference;
 
-
-	public IPCInterfaceImpl ( Object _target ) {
-		target = _target;
+	public IPCInterfaceImpl(Object _target) {
+		reference = new WeakReference<Object>(_target);
 	}
 
-	public Object invoke( String methodName, Object[] params )
-	throws IPCException {
+	public Object invoke(String methodName, Object[] params)
+			throws IPCException {
 
+		Object target = reference.get();
+		if (target == null) {
+			throw new IPCException(
+					"Invalid Operation. Target was already recycled.");
+		}
 		try {
+
+			if (params == null) {
+				params = new Object[0];
+			}
+
 			Class[] paramTypes = new Class[params.length];
-			for (int i=0;i<params.length;i++) {
+			for (int i = 0; i < params.length; i++) {
 				if (params[i] instanceof Boolean) {
 					paramTypes[i] = boolean.class;
 				} else if (params[i] instanceof Integer) {
@@ -41,43 +51,45 @@ public class IPCInterfaceImpl implements IPCInterface {
 					paramTypes[i] = char.class;
 				} else if (params[i] instanceof Short) {
 					paramTypes[i] = short.class;
-				} else
+				} else {
 					paramTypes[i] = params[i].getClass();
+				}
 			}
 
-			Method mtd	= null;
+			Method mtd = null;
 
-			try{
-				mtd = target.getClass().getMethod(methodName,paramTypes);
+			try {
+				mtd = target.getClass().getMethod(methodName, paramTypes);
 
-			}catch( NoSuchMethodException e ){
+			} catch (NoSuchMethodException e) {
 
-				Method[]	methods = target.getClass().getMethods();
+				Method[] methods = target.getClass().getMethods();
 
-				for (int i=0;i<methods.length;i++){
+				for (int i = 0; i < methods.length; i++) {
 
-					Method	method = methods[i];
+					Method method = methods[i];
 
 					Class[] method_params = method.getParameterTypes();
 
-					if ( method_params.length == paramTypes.length ){
+					if (method.getName().equals(methodName)
+							&& method_params.length == paramTypes.length) {
 
-						boolean	ok = true;
+						boolean ok = true;
 
-						for (int j=0;j<method_params.length;j++){
+						for (int j = 0; j < method_params.length; j++) {
 
-							Class	declared 	= method_params[j];
-							Class	supplied	= paramTypes[j];
+							Class declared = method_params[j];
+							Class supplied = paramTypes[j];
 
-							if ( !declared.isAssignableFrom( supplied )){
+							if (!declared.isAssignableFrom(supplied)) {
 
-								ok	= false;
+								ok = false;
 
 								break;
 							}
 						}
 
-						if ( ok ){
+						if (ok) {
 
 							mtd = method;
 
@@ -86,9 +98,9 @@ public class IPCInterfaceImpl implements IPCInterface {
 					}
 				}
 
-				if ( mtd == null ){
+				if (mtd == null) {
 
-					throw( e );
+					throw (e);
 				}
 			}
 
